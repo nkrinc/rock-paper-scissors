e15afd6ff698a35383e338b167749b829262264b
nkrinc <65736937+nkrinc@users.noreply.github.com>
2020-06-03 11:18:15 +0100

    Create rockpaperscissors.rb

---
+++ rockpaperscissors.rb
@@ -0,0 +1,24 @@
+ puts "Rock, paper scissors! choose one!"
+  choices = ["rock","paper","scissors"]
+  choice = gets.chomp
+  choice = choice.downcase
+  cpu = choices.sample  
+  lose = "You lost! cpu chose " + cpu
+  win = "You win! cpu chose " + cpu
+if cpu == choice
+  puts "It's a tie! cpu chose " + cpu
+end
+if cpu == "rock" && choice == "scissors"
+  puts lost
+
+elsif cpu == "rock" && choice == "paper"
+  puts win
+elsif cpu == "paper" && choice == "rock"
+  puts lose
+elsif cpu == "paper" && choice == "scissors"
+  puts win
+elsif cpu == "scissors" && choice == "rock"
+  puts win
+elsif cpu == "scissors" && choice == "paper"
+  puts lost
+end
