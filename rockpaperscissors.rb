a6995209b6acbce1e89bd39cb13271bdd1b6edcc
nkrinc <65736937+nkrinc@users.noreply.github.com>
2020-06-03 12:52:49 +0100

    Update rockpaperscissors.rb

--- rockpaperscissors.rb
+++ rockpaperscissors.rb
@@ -1,8 +1,9 @@
- puts "Rock, paper scissors! choose one!"
+def game
+  puts "Rock, paper scissors! choose one!"
   choices = ["rock","paper","scissors"]
   choice = gets.chomp
   choice = choice.downcase
-  cpu = choices.sample  
+  cpu = choices.sample  #.sample chooses randomly from the array
   lose = "You lost! cpu chose " + cpu
   win = "You win! cpu chose " + cpu
 if cpu == choice
@@ -21,4 +22,8 @@ elsif cpu == "scissors" && choice == "rock"
   puts win
 elsif cpu == "scissors" && choice == "paper"
   puts lose
+end
+end
+loop do
+  game
 end
